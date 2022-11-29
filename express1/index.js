0715772790e036ddc94691a0fada9fdb20e919c5
yuvraj3905 <yuvraj202001@gmail.com>
2022-11-29 11:10:07 +0530

    Update index.js

--- express1/index.js
+++ express1/index.js
@@ -1,11 +1,13 @@
-
-
-
 let express = require('express');
-let app=express();
+let app = express();
 
-console.log(app);
-
-app.listen(3001, ()=>{ 
-    console.log("hi server is connected");
-})
+// console.log(app);
+app.use((req, res) => {
+  console.log(req);
+  console.log(res);
+  console.log('hi i am a middleware funciton');
+  res.send('<h1>hi welcome to the server</h1>');
+});
+app.listen(3001, () => {
+  console.log('hi server is connected');
+});
