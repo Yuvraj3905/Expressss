875d4b314e49e46d9da3eec4d8c703bedd3541d4
yuvraj3905 <yuvraj202001@gmail.com>
2022-12-04 12:16:36 +0530

    Update nav.js

--- express1/nav.js
+++ express1/nav.js
@@ -1,14 +1,20 @@
-const express=require('express');
+const express = require('express');
 const app = express();
-const port=7000;
+const port = 7000;
 
-app.get('/', (req, res)=>{
-    res.send('hello from Home Page');
+app.get('/', (req, res) => {
+  res.write('<h1>hello from Home Page</h1>');//here when we use write the page keeps on loading to expect more data changes until send is used
+  res.send();
 });
-app.get('/about', (req, res)=>{
-    res.send('hello from About Page');
+app.get('/about', (req, res) => {
+  res.send('hello from About Page');
 });
+app.get('/temp', (req, res) => {
+    res.send({ id:1, 
+    name:"vinod"
+});
+  });
 
-app.listen(port,()=>{
-    console.log(`listening on port no ${port}`);
+app.listen(port, () => {
+  console.log(`listening on port no ${port}`);
 });
