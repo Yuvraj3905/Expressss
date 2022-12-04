94690f9d441d7313e29904cf10932b201dbdf791
yuvraj3905 <yuvraj202001@gmail.com>
2022-12-04 16:26:43 +0530

    Using Templates_hbs, pugs

--- express1/src/index.js
+++ express1/src/index.js
@@ -1,37 +1,39 @@
-const path=require('path');
-const express = require('express');
+// const path=require('path');
+// const express = require('express');
 
-const app = express();
-//relative path nhi dena absolute dena hai
-// console.log(__dirname);
+// const app = express();
+// //relative path nhi dena absolute dena hai
+// // console.log(__dirname);
 
-const staticPath=path.join(__dirname, "../public");
+// const staticPath=path.join(__dirname, "../public");
 
 
-//builtin middleware
-app.use(express.static(staticPath));
-app.get('/', (req, res) => {
-  res.send('hello from express123456');
-}); //get(root,callback)  CALLBACK functions have firther 2 parameters req,res
+// //builtin middleware
+// app.use(express.static(staticPath));
+// app.get('/', (req, res) => {
+//   res.send('hello from express123456');
+// }); //get(root,callback)  CALLBACK functions have firther 2 parameters req,res
 
-app.get('/about', (req, res) => {
-  res.send('hello from ABOUT');
-});
-
-app.listen(8000, () => {
-  console.log('listening on port at 8000');
-});
-
-// let express = require('express');
-// let app = express();
-
-// // console.log(app);
-// app.use((req, res) => {
-//   console.log(req);
-//   console.log(res);
-//   console.log('hi i am a middleware funciton');
-//   res.send('<h1>hi welcome to the server</h1>');
+// app.get('/about', (req, res) => {
+//   res.send('hello from ABOUT');
 // });
-// app.listen(3001, () => {
-//   console.log('hi server is connected');
+
+// app.listen(8000, () => {
+//   console.log('listening on port at 8000');
 // });
+
+// // let express = require('express');
+// // let app = express();
+
+// // // console.log(app);
+// // app.use((req, res) => {
+// //   console.log(req);
+// //   console.log(res);
+// //   console.log('hi i am a middleware funciton');
+// //   res.send('<h1>hi welcome to the server</h1>');
+// // });
+// // app.listen(3001, () => {
+// //   console.log('hi server is connected');
+// // });
+
+
