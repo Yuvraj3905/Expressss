478936a0f17cbdd1eddd41c3621feabcd7fb16e2
yuvraj3905 <yuvraj202001@gmail.com>
2022-12-04 16:11:34 +0530

    builtin  middleware

--- express1/src/index.js
+++ express1/src/index.js
@@ -1,7 +1,15 @@
+const path=require('path');
 const express = require('express');
 
 const app = express();
+//relative path nhi dena absolute dena hai
+// console.log(__dirname);
 
+const staticPath=path.join(__dirname, "../public");
+
+
+//builtin middleware
+app.use(express.static(staticPath));
 app.get('/', (req, res) => {
   res.send('hello from express123456');
 }); //get(root,callback)  CALLBACK functions have firther 2 parameters req,res
